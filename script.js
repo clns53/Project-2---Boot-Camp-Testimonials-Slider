f060026367d4c11cfe0115a571c40421a0f744f8
clns53 <ckomutai@gmail.com>
2022-07-18 10:20:54 +0300

    slight changes on favicon and the slides behaviour

--- script.js
+++ script.js
@@ -5,6 +5,10 @@ function plusSlides(n) {
   showSlides(slideIndex += n);
 }
 
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+  }
+
 function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName("theslides");
